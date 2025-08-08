5b14cc7739adf191c7ec0e565a715fdee915e7aa
Thisara260 <thisaragimhana011@gmail.com>
2025-08-08 07:36:27 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -2,9 +2,9 @@
 module.exports = {
   SESSION_ID: "",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "94704198014", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "COLOMBO" //put your country timeZone....leave blank if u don't know.
 };
